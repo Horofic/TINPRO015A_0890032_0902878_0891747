1401b4eb679498fa66b86114d0ec3815b445fd37
Horofic <ikweetnie123@hotmail.com>
2015-09-08 13:55:47 +0200

    Tested MonoGame on my computer

--- UltimatePong/UltimatePong/Game1.cs
+++ UltimatePong/UltimatePong/Game1.cs
@@ -59,7 +59,7 @@ namespace UltimatePong
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
