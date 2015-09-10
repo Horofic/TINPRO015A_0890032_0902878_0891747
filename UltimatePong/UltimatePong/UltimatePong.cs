1bdc19748259aea4dbc7c97c71540f15c353e8a8
Horofic <ikweetnie123@hotmail.com>
2015-09-10 17:27:03 +0200

    ADD smarter border / player collision detection

--- UltimatePong/UltimatePong/UltimatePong.cs
+++ UltimatePong/UltimatePong/UltimatePong.cs
@@ -217,7 +217,7 @@ namespace UltimatePong
                 bottomBar.Offset(bottomBarSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds, 0);
 
             // Border collision detection
-            BorderCollision(ball);
+            checkBallCollision();
 
 
 
@@ -280,7 +280,7 @@ namespace UltimatePong
             {
                 return emptyRectangle;
             }
-
+            // check if ball touches a player a.k.a bar
             else if (ball.Intersects(topBar))
                 return topBar;
             else if (ball.Intersects(bottomBar))
@@ -290,27 +290,11 @@ namespace UltimatePong
             else if (ball.Intersects(rightBar))
                 return rightBar;
 
+
+            // check if ball touches the border if does that player loses a life and ball is reset
             else if (ball.Intersects(topBorder))
-                return topBorder;
-            else if (ball.Intersects(bottomBorder))
-                return bottomBorder;
-            else if (ball.Intersects(leftBorder))
-                return leftBorder;
-            else if (ball.Intersects(rightBorder))
-                return rightBorder;
-
-            else return emptyRectangle;
-        }
-
-
-
-        protected void BorderCollision(Rectangle checkBall)
-        {
-            if (checkBall.Intersects(topBorder))
             {
-                System.Console.WriteLine("Top border hit");
-
-                topPlayerLives = topPlayerLives- 1;
+                topPlayerLives = topPlayerLives - 1;
 
                 if (topPlayerLives == 0)
                 {
@@ -318,25 +302,55 @@ namespace UltimatePong
                 }
 
                 ResetBall();
-            }
 
-            if (checkBall.Intersects(rightBorder))
-            {
-                System.Console.WriteLine("Right border hit");
-                ResetBall();
+                return topBorder;
             }
+               
+            else if (ball.Intersects(bottomBorder))
+            {
+                bottomPlayerLives = bottomPlayerLives - 1;
 
-            if (checkBall.Intersects(leftBorder))
-            {
-                System.Console.WriteLine("Left border hit");
-                ResetBall();
-            }
+                if (bottomPlayerLives == 0)
+                {
+                    bottomBar.Offset(800, 800);
+                }
 
-            if (checkBall.Intersects(bottomBorder))
-            {
-                System.Console.WriteLine("Bottom border hit");
                 ResetBall();
+
+
+                return bottomBorder;
             }
+                
+            else if (ball.Intersects(leftBorder))
+            {
+                leftPlayerLives = leftPlayerLives - 1;
+
+                if (leftPlayerLives == 0)
+                {
+                    leftBar.Offset(800, 800);
+                }
+
+                ResetBall();
+
+                return leftBorder;
+            }
+                
+            else if (ball.Intersects(rightBorder))
+            {
+                rightPlayerLives = rightPlayerLives - 1;
+
+                if (rightPlayerLives == 0)
+                {
+                    rightBar.Offset(800, 800);
+                }
+
+                ResetBall();
+
+                return rightBorder;
+            }
+                
+
+            else return emptyRectangle;
         }
 
 
