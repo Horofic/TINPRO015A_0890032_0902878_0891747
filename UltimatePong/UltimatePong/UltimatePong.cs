7574d189a7b79b7fbcc9c03e4e9b316b485938d8
Terry van der Jagt <terryvanderjagt@gmail.com>
2015-10-29 13:03:19 +0100

    added maxPowerupCount logic

--- UltimatePong/UltimatePong/UltimatePong.cs
+++ UltimatePong/UltimatePong/UltimatePong.cs
@@ -83,6 +83,8 @@ namespace UltimatePong
         public bool powerupEnabled;
         private bool firstCycle;
         private bool gameDone;
+        private int maxPowerupCount;
+
 
         //powerup
         Power_up power_up = new Power_up();
@@ -320,53 +322,34 @@ namespace UltimatePong
             switch (gameLogic.Execute(deltaTime))
             {
                 case InstructionResult.DoneAndCreatePowerup:
-                    switch(random.Next(0,5))
-                    {
-                        case 0:
-                            newPowerups.Add(new GreenPowerupController(barTexture, spriteBatch));
-                            break;
-                        case 1:
-                            newPowerups.Add(new RedPowerupController(barTexture, spriteBatch));
-                            break;
-                        case 2:
-                            newPowerups.Add(new GoldPowerupController(barTexture, spriteBatch));
-                            break;
-                        case 3:
-                            newPowerups.Add(new PinkPowerupController(barTexture, spriteBatch));
-                            break;
-                        case 4:
-                            newPowerups.Add(new BluePowerupController(barTexture, spriteBatch));
-                            break;
-                        default:
-                            break;
-                    }
-
-                    Console.WriteLine("newPowerup done");
-                    break;
                 case InstructionResult.RunningAndCreatePowerup:
-                    switch (random.Next(0, 5))
+                    if (newPowerups.Count < maxPowerupCount)
                     {
-                        case 0:
-                            newPowerups.Add(new GreenPowerupController(barTexture, spriteBatch));
-                            break;
-                        case 1:
-                            newPowerups.Add(new RedPowerupController(barTexture, spriteBatch));
-                            break;
-                        case 2:
-                            newPowerups.Add(new GoldPowerupController(barTexture, spriteBatch));
-                            break;
-                        case 3:
-                            newPowerups.Add(new PinkPowerupController(barTexture, spriteBatch));
-                            break;
-                        case 4:
-                            newPowerups.Add(new BluePowerupController(barTexture, spriteBatch));
-                            break;
-                        default:
-                            break;
+                        switch (random.Next(0, 5))
+                        {
+                            case 0:
+                                newPowerups.Add(new GreenPowerupController(barTexture, spriteBatch));
+                                break;
+                            case 1:
+                                newPowerups.Add(new RedPowerupController(barTexture, spriteBatch));
+                                break;
+                            case 2:
+                                newPowerups.Add(new GoldPowerupController(barTexture, spriteBatch));
+                                break;
+                            case 3:
+                                newPowerups.Add(new PinkPowerupController(barTexture, spriteBatch));
+                                break;
+                            case 4:
+                                newPowerups.Add(new BluePowerupController(barTexture, spriteBatch));
+                                break;
+                            default:
+                                break;
+                        }
                     }
-
-
-                    Console.WriteLine("newPowerup running");
+                    else
+                    {
+                        newPowerups.RemoveAt(0);
+                    }
                     break;
             }
             int playersAlive = 0;
